bc8b73da7b678a655d09379e0e2b5c683e462cae
Javier Preciado <jpreciado85@gmail.com>
2025-01-25 16:57:22 -0500

    modificación del tiempo de animación del contenido AOS

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
+AOS.init({
+  duration: 500, 
+  easing: "ease-in-out",
+});
 
-      AOS.init();
-
-      // Carousel Initialization
-      $(document).ready(function () {
-        $(".carousel").slick({
-          autoplay: true,
-          autoplaySpeed: 3000,
-          dots: true,
-        });
-      });
-
-      // Contact Form Validation
-      document
-        .getElementById("contactForm")
-        .addEventListener("submit", function (event) {
-          event.preventDefault();
-          Swal.fire(
-            "¡Gracias por tu mensaje!",
-            "Nos pondremos en contacto pronto.",
-            "success"
-          );
-        });
-
-      // Initialize Slick Carousel
-      $("#testimonialCarousel").slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: true,
-      });
+// Carousel Initialization
+$(document).ready(function () {
+    $(".carousel").slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+    });
+    
+    $("#testimonialCarousel").slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: true,
+    });
+  });
   
+
+// Contact Form Validation
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    Swal.fire(
+      "¡Gracias por tu mensaje!",
+      "Nos pondremos en contacto pronto.",
+      "success"
+    );
+  });
+
